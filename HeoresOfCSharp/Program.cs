28c077c89f667a88549334040895a839b20c1bf1
rubikshift <michal.krak97@gmail.com>
2017-05-10 15:35:41 +0200

    Add project files.

---
+++ HeoresOfCSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace HeoresOfCSharp
+{
+    class Program
+    {
+        static void OnClose(object sender, EventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+            window.Close();
+        }
+
+        static void Main()
+        {
+            RenderWindow app = new RenderWindow(new VideoMode(800, 600), "SFML Works!");
+            app.Closed += new EventHandler(OnClose);
+
+            Color windowColor = new Color(0, 192, 255);
+            while (app.IsOpen)
+            {
+                app.DispatchEvents();
+                app.Clear(windowColor);
+                app.Display();
+            }
+        }
+    }
+}
